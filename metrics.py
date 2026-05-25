ffc9887219b2a091a1a347f22b10c11e80a8ea42
June Yu <yutimwin@gmail.com>
2026-05-25 09:03:52 -0600

    Flatness calculation by ratio

--- metrics.py
+++ metrics.py
@@ -1,21 +1,21 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from pylinac.metrics.profile import ProfileMetric
 import pydicom
 
 from pylinac import FieldProfileAnalysis, Centering, Normalization, Edge
 from pylinac.metrics.profile import (
+    ProfileMetric,
     PenumbraLeftMetric,
     PenumbraRightMetric,
     SymmetryAreaMetric,
     FlatnessDifferenceMetric,
+    FlatnessRatioMetric
 )
 
 # Finds the value of the central axis (CAX) by taking the average of the two middle values in the profile. This is a common method for finding the CAX value in a profile, as it is less sensitive to noise than taking the maximum value.
 def get_cax_value(values: np.ndarray) -> float:
     length = len(values)
-    print("mid value:", (values[length // 2] + values[length // 2 - 1]) / 2)
     return (values[length // 2] + values[length // 2 - 1]) / 2
 
 # Finds the indices of the left and right transitions in the profile, where the values cross a specified threshold. The left transition is found by iterating from the beginning of the profile until a value greater than the threshold is found, while the right transition is found by iterating from the end of the profile until a value greater than the threshold is found. If either transition is not found, a ValueError is raised.
@@ -62,11 +62,6 @@ class FlatnessCalculationByVariance(ProfileMetric):
         left_roi_index = (int) (cax_index - (cax_index - left_field_index) * self.in_field_ratio)
         right_roi_index = (int) (cax_index + (right_field_index - cax_index) * self.in_field_ratio)
 
-        print(cax_value)
-        print("indices:", left_field_index, right_field_index)
-        print("roi indices:", left_roi_index, right_roi_index)
-        print(self.profile.field_width_px)
-        print(self.profile.field_indices(in_field_ratio=self.in_field_ratio))
         return (
             100
             * (values[left_roi_index:right_roi_index+1].max() - values[left_roi_index:right_roi_index+1].min())
@@ -81,14 +76,40 @@ class FlatnessCalculationByRatio(ProfileMetric):
         super().__init__(color=color, linestyle=linestyle)
 
     def calculate(self) -> float:
-        """Calculate the flatness ratio of the profile."""
+        
+        values = self.profile.values
+        cax_value = get_cax_value(values)
+        fifty_percent_value = cax_value * 0.5
+        cax_index = self.profile.cax_index
+        left_field_index, right_field_index = get_transition_indices(values, fifty_percent_value)
+        if left_field_index > cax_index or right_field_index < cax_index:
+            raise ValueError("CAX index is not between the left and right field indices.")
+
+        left_length_mm = (cax_index - left_field_index) / self.profile.dpmm
+        right_length_mm = (right_field_index - cax_index) / self.profile.dpmm
+
+        if left_length_mm < 100:
+            left_roi_index = (int) (cax_index - (left_length_mm - 20) * self.profile.dpmm)
+        elif left_length_mm < 300:
+            left_roi_index = (int) (cax_index - (left_length_mm * 0.8) * self.profile.dpmm)
+        else:
+            left_roi_index = (int) (cax_index - (left_length_mm - 60) * self.profile.dpmm)
+
+        if right_length_mm < 100:
+            right_roi_index = (int) (cax_index + (right_length_mm - 20) * self.profile.dpmm)
+        elif right_length_mm < 300:
+            right_roi_index = (int) (cax_index + (right_length_mm * 0.8) * self.profile.dpmm)
+        else:
+            right_roi_index = (int) (cax_index + (right_length_mm - 60) * self.profile.dpmm)
+        print ("left_roi_index:", left_roi_index)
+        print ("right_roi_index:", right_roi_index)
         return (
             100
-            * (self.profile.field_values().max() - self.profile.field_values().min())
-            / (self.profile.field_values().max() + self.profile.field_values().min())
+            * (values[left_roi_index:right_roi_index+1].max())
+            / (values[left_roi_index:right_roi_index+1].min())
         )
     
-
+### TESTING DELETE LATER
 path = 'Solstice-m12_d18_2025-FS_EPID_MLC 10x38.dcm'
 field_analyzer = FieldProfileAnalysis(path)
 field_analyzer.analyze(
@@ -104,6 +125,8 @@ field_analyzer.analyze(
         SymmetryAreaMetric(),
         FlatnessDifferenceMetric(),
         FlatnessCalculationByVariance(),
+        FlatnessRatioMetric(),
+        FlatnessCalculationByRatio()
     ),
 )
 print(field_analyzer.results())
